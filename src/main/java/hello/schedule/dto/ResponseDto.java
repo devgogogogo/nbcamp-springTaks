8785bdad3a88947ca4885409d2ba67b2c9cc993b
devgogogogo <imleo322@gmail.com>
2025-03-26 13:53:44 +0900

    CRUD 추가 수정 (POST)

--- src/main/java/hello/schedule/dto/ResponseDto.java
+++ src/main/java/hello/schedule/dto/ResponseDto.java
@@ -1,9 +1,20 @@
 package hello.schedule.dto;
 
+import hello.schedule.entity.Todo;
+import lombok.Getter;
+
+@Getter
 public class ResponseDto {
 
     private String name;
     private String title;
     private String todo;
     private Integer date;
+
+    public ResponseDto(Todo todo) {
+        this.name = todo.getName();
+        this.title = todo.getTitle();
+        this.todo = todo.getTodo();
+        this.date = todo.getDate();
+    }
 }
